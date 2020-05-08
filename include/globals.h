c79b70f83edfb336f2228953f5f26a4fe5183817
Dmitry <work.dmitry.shpakov@gmail.com>
2020-05-08 13:05:14 +0300

    UART init routine updated

---
+++ include/globals.h
@@ -0,0 +1,5 @@
+enum PARITY { NO, ODD, EVEN}
+#define UART_BAUD_RATE 57600
+#define UART_BITS 8	// only 8-bit character size supported at the moment
+#define UART_PARITY NO
+#define UART_STOP_BITS 1
--- source/hardware.c
+++ source/hardware.c
@@ -1,3 +1,4 @@
+#include "gloabals.h"
 #include "hardware.h"
 
 
@@ -10,7 +11,32 @@ void initIO() {
 	PORTB &= ~( SRCLK_MASK | SER_MASK | SRCLR_N_MASK | RCLK_MASK | OE_N_MASK );
 }
 
-void initUART();
+void initUART() {
+
+	UCSRB |= (1 << RX);
+	#if UART_PARITY == NO
+		UCSRC &= ~( (1 << UPM1) | (1 << UPM0));
+	#elif UART_PARITY == ODD
+		UCSRC |= (1 << UPM1) | (1 << UPM0);
+	#elif UART_PARITY == EVEN
+		UCSRC |= (1 << UPM1);
+		UCSRC &= ~(1 << UPM0);
+	#else
+		#error UART parity not specified or is incorrect
+	#endif
+
+	#if UART_BITS != 8
+		#error UART 8bit character size only supported
+	#else
+		UCSRC |= (1 << UCSZ1) | (1 << UCSZ0);
+		UCSRC &= ~(1 << UCSZ2);
+	#endif
+
+	
+
+
+
+}
 
 void initJiffy();
 
