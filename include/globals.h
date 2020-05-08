f60e6c4d3a322238b9ea7429ce0e887f323d0e26
Dmitry <work.dmitry.shpakov@gmail.com>
2020-05-08 16:22:26 +0300

    globals.h update

--- include/globals.h
+++ include/globals.h
@@ -1,5 +1,14 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdint.h>
+
 enum PARITY { NO, ODD, EVEN};
 #define UART_BAUD_RATE 57600
 #define UART_BITS 8	// only 8-bit character size supported at the moment
 #define UART_PARITY NO
 #define UART_STOP_BITS 1
+
+extern volatile uint32_t jiffy;
+
+#endif	// GLOBALS_H
